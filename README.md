3847b65b4ddb36f6255886563d3edcd35786db6d
刘龙彬 <liulongbin1314@outlook.com>
2020-07-05 20:04:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,91 @@
+## 安装
+
+```bash
+npm install @escook/request-miniprogram
+```
+
+## 导入
+
+```js
+// 按需导入 $http 对象
+import { $http } from 'request-mp'
+
+// 将按需导入的 $http 挂载到 wx 顶级对象之上，方便全局调用
+wx.$http = $http
+```
+
+## 使用
+
+### 支持的请求方法
+
+```js
+// 发起 GET 请求，data 是可选的参数对象
+$http.get(url, data?)
+// 发起 POST 请求，data 是可选的参数对象
+$http.post(url, data?)
+// 发起 PUT 请求，data 是可选的参数对象
+$http.put(url, data?)
+// 发起 DELETE 请求，data 是可选的参数对象
+$http.delete(url, data?)
+```
+
+### 配置请求根路径
+
+```js
+$http.baseUrl = 'https://www.example.com'
+```
+
+### 请求拦截器
+
+```js
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // do somethimg...
+}
+```
+
+例 1，展示 loading 效果：
+
+```js
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  wx.showLoading({
+    title: '数据加载中...',
+  })
+}
+```
+
+例 2，自定义 header 请求头：
+
+```js
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  if (options.url.indexOf('/home/catitems') !== -1) {
+    options.header = {
+      'X-Test': 'AAA',
+    }
+  }
+}
+```
+
+### 响应拦截器
+
+```js
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  // do something...
+}
+```
+
+例如，隐藏 loading 效果：
+
+```js
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  wx.hideLoading()
+}
+```
+
+## 开源协议
+
+![MIT](https://img.shields.io/badge/License-MIT-blue)
---
+++ miniprogram_dist/index.js
@@ -0,0 +1,68 @@
+class Request {
+  constructor(options = {}) {
+    // 请求的根路径
+    this.baseUrl = options.baseUrl || ''
+    // 请求的 url 地址
+    this.url = options.url || ''
+    // 请求方式
+    this.method = 'GET'
+    // 请求的参数对象
+    this.data = null
+    // header 请求头
+    this.header = options.header || {}
+    this.beforeRequest = null
+    this.afterRequest = null
+  }
+
+  get(url, data = {}) {
+    this.method = 'GET'
+    this.url = this.baseUrl + url
+    this.data = data
+    return this._()
+  }
+
+  post(url, data = {}) {
+    this.method = 'POST'
+    this.url = this.baseUrl + url
+    this.data = data
+    return this._()
+  }
+
+  put(url, data = {}) {
+    this.method = 'PUT'
+    this.url = this.baseUrl + url
+    this.data = data
+    return this._()
+  }
+
+  delete(url, data = {}) {
+    this.method = 'DELETE'
+    this.url = this.baseUrl + url
+    this.data = data
+    return this._()
+  }
+
+  _() {
+    // 清空 header 对象
+    this.header = {}
+    // 请求之前做一些事
+    this.beforeRequest && typeof this.beforeRequest === 'function' && this.beforeRequest(this)
+    // 发起请求
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: this.url,
+        method: this.method,
+        data: this.data,
+        header: this.header,
+        success: (res) => { resolve(res) },
+        fail: (err) => { reject(err) },
+        complete: (res) => {
+          // 请求完成以后做一些事情
+          this.afterRequest && typeof this.afterRequest === 'function' && this.afterRequest(res)
+        }
+      })
+    })
+  }
+}
+
+export const $http = new Request()
